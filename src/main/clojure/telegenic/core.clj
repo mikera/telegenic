e7adf639a0b4f9c7bc6ba041226ed2af08ef4ef2
mikera <mike.r.anderson.13@gmail.com>
2015-09-28 16:42:35 +0800

    Work on frame grab code

--- src/main/clojure/telegenic/core.clj
+++ src/main/clojure/telegenic/core.clj
@@ -1,3 +1,21 @@
 (ns telegenic.core
-  (:import [org.jcodec.api FrameGrab]))
+  (:import [org.jcodec.api FrameGrab])
+  (:import [java.io File])
+  (:import [java.awt.image BufferedImage])
+  (:import [org.jcodec.common.model Picture])
+  )
 
+(set! *warn-on-reflection* true)
+(set! *unchecked-math* true)
+
+(defprotocol PGetFrame
+  (get-frame [video frame-number]))
+
+(extend-protocol PGetFrame
+  String
+    (get-frame [s frame-number] (get-frame (File. s) frame-number))
+  File
+    (get-frame [file frame-number]
+      (let [^Picture nf (FrameGrab/getNativeFrame file (int frame-number))
+            bi (.toBufferedImage nf)]
+        bi)))
