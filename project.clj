efc5d13693438d4823b9ecdc2bd7258c0fbbedda
tom <tom@that-place>
2016-06-30 23:58:57 -0500

    Changed to 0.2.0 version, built locally

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject telegenic "0.0.2"
+  :description "Fork of telegenic by Mikera"
+  :url "https://github.com/mikera/telegenic"
+  :license {:name "GNU LESSER GENERAL PUBLIC LICENSE
+                   Version 2.1, February 1999"}
+  :plugins [[lein-localrepo "0.5.3"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.jcodec/jcodec        "0.2.0"]
+;                 [org.jcodec/jcodec-javase "0.2.0"]
+                 ])
